8e2fb2461cd2f50ed6e357b57ee02e40d1098314
takagotch <dyaccb@gmail.com>
2018-01-30 20:44:50 +0900

    xhr.open('GET', 'index_ajax.js' + encodeURIComponent(), true); xhr.send(null);

---
+++ XMLHttpRequest.js
@@ -0,0 +1,23 @@
+*response
+*readyState
+*responseText
+responseType
+*responseXML
+*status
+*statusText
+timeout
+withCredentials
+onreadystatechange
+ontimeout
+
+abort()
+**getAllResponseHearderes()
+**getResponseHeader(header)
+open(...)
+send(body)
+setRequestHeader(header, value)
+
+xhr.open(method, url [, async [,user [,passwd]]])
+xhr,open('GET', 'index_ajax.php?name' +
+  encodingURIComponent(document.getElementById('name').value), true);
+
--- index_ajax.js
+++ index_ajax.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded', function(){
   document.getElementById('btn').addEventListener('click', function(){
     var result = document.getElementById('result');
     var xhr = new XMLHttpRequest();
+//1
     xhr.onreadystatechange = function(){
       if(xhr.readyState === 4){
         if(xhr.status){
@@ -17,6 +18,19 @@ document.addEventListener('DOMContentLoaded', function(){
     xhr.open('GET', 'index_ajax.php?name=' +
       encodeURIComponent(document.getElementById('search').value), true);
     xhr.send(null);
-    }, false);
   }, false);
+}, false);
+
+//2
+xhr.addEventListener('loadstart', function(){
+  result.textContent = 'LOADING...';
+}, false);
+
+xhr.addEventListener('load', function(){
+  result.textContent = xhr.responseText;
+}, false);
+
+xhr.addEventListener('error', function(){
+  result.textContent = 'EER';
+}, false);
 
